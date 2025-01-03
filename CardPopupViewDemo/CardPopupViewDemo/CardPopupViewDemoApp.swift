6f7fa152830b952f702c2e9e9d3fbacb89e3f260
Hishara Dilshan <hisharadilshan3@gmail.com>
2025-01-03 23:41:16 +0000

    Initial Commit

---
+++ CardPopupViewDemo/CardPopupViewDemo/CardPopupViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardPopupViewDemoApp.swift
+//  CardPopupViewDemo
+//
+//  Created by Hishara Dilshan on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CardPopupViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
